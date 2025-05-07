8a56f68f9faedc5b928e6657b0d163981e1c0bb2
Taran Warnock <taran.warnock@usask.ca>
2025-05-07 17:59:19 -0600

    debug

--- build.rs
+++ build.rs
@@ -27,7 +27,7 @@ fn compile_mt_ckd() {
     let nc_include = env::var("NCI").unwrap_or_else(|_| default_include.to_string());
     let nc_lib = env::var("NCL").unwrap_or_else(|_| default_lib.to_string());
     let FC = env::var("FC").unwrap_or_else(|_| "gfortran".to_string());
-    let PATH = env::var("PATH");
+    let PATH = env::var("PATH").unwrap_or_else(|_| "".to_string());
 
     let status = Command::new("make")
         .current_dir("build_scripts/MT_CKD")
